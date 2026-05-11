af13ef70d8adabdc92ed73738ca575dba47ad1eb
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-05-10 22:24:45 -0500

    Update hello.cpp

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,77 @@
 #include <string>
+#include <iostream>
 
-std::string hello() {
-   return "Hello World!";
-}
+
+#include "hello.hpp"
+
+
+using namespace std;
+
+
+
+Squirrel *NodeClass::get_squirrel() {
+    return this->squirrel;
+};
+
+NodeClass::NodeClass(Squirrel *squirrel) {
+    this->squirrel = squirrel;
+    this->leftnode = nullptr;
+    this->rightnode = nullptr;
+};
+
+
+
+
+
+
+
+
+void NodeClass::rightside(NodeClass* right) {
+    this->rightnode = right;
+};
+
+
+NodeClass* NodeClass::leftobject() {
+    return this->leftnode;
+};
+
+
+
+void NodeClass::leftside(NodeClass* left) {
+    this->leftnode = left;
+};
+
+NodeClass* NodeClass::rightobject() {
+  return this->rightnode;
+};
+
+
+TreeClass::TreeClass(NodeClass *roottreeobject) {
+    this->roottreeobject = roottreeobject;
+};
+
+
+NodeClass *TreeClass::getrootclass() {
+    return this->roottreeobject;
+};
+
+
+void TreeClass::consoleoutputtreeclass() {
+    consolenodelist(this->roottreeobject);
+};
+
+
+void TreeClass::consolenodelist(NodeClass *nodeclass) {
+  if (nodeclass == nullptr) {
+      return;
+  }
+
+
+    std::cout<<nodeclass->get_squirrel()->get_name()<<std::endl;
+    consolenodelist(nodeclass->leftobject());
+    consolenodelist(nodeclass->rightobject());
+
+
+
+
+};
