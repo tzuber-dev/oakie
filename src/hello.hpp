62892eae88ee042693f92a94f4180a0235a5de60
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-05-10 22:23:59 -0500

    Update hello.hpp

--- src/hello.hpp
+++ src/hello.hpp
@@ -1,3 +1,68 @@
 #include <string>
 
-std::string hello();
+#include <iostream>
+
+
+using namespace std;
+
+
+class Squirrel {
+private:
+    string name;
+
+public:
+    Squirrel(string name) {
+        this->name = name;
+    }
+
+    string get_name() {
+        return this->name;
+    }
+};
+
+
+
+
+
+
+class NodeClass {
+    private:
+    Squirrel* squirrel;
+    NodeClass* leftnode;
+    NodeClass* rightnode;
+
+
+public:
+    NodeClass(Squirrel* squirrel);
+    Squirrel* get_squirrel();
+
+
+    void leftside(NodeClass* left);
+    void rightside(NodeClass* right);
+
+    NodeClass* leftobject();
+    NodeClass* rightobject();
+
+
+
+};
+
+
+
+
+class TreeClass {
+    private:
+    NodeClass* roottreeobject;
+
+
+
+public:
+
+    TreeClass(NodeClass* roottreeobject);
+    NodeClass* getrootclass();
+
+    void consoleoutputtreeclass();
+    void consolenodelist(NodeClass* nodeclass);
+
+
+};
